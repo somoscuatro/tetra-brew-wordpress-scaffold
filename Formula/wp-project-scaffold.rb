ef6490a14019b216a7f5cbe4375e83225048938d
Luca Pipolo <luca@somoscuatro.es>
2025-12-05 16:07:08 +0100

    chore: bump project version

--- Formula/wp-project-scaffold.rb
+++ Formula/wp-project-scaffold.rb
@@ -1,7 +1,7 @@
 class WpProjectScaffold < Formula
   desc "Scaffolds a WordPress project using Docker and optionally a starter theme"
   homepage "https://github.com/somoscuatro/tetra-brew-wordpress-scaffold"
-  url "https://github.com/somoscuatro/tetra-brew-wordpress-scaffold/archive/refs/tags/v1.2.0.tar.gz"
+  url "https://github.com/somoscuatro/tetra-brew-wordpress-scaffold/archive/refs/tags/v1.2.1.tar.gz"
   sha256 "3618c13f19629ecc43ce7668da7980b1e18c7df9138bc7961e1a4ffc37507c12"
   license "MIT"
 
